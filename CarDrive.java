1c4e510f78e27a25baa43aa029bfe341be0ff81d
Md Mehedi Hasan <Md Mehedi Hasan>
2017-04-25 18:59:42 -0600

    first commit

--- CarDrive.java
+++ CarDrive.java
@@ -1,12 +1,13 @@
+import javax.swing.JOptionPane;
 
 public class CarDrive {
 public static void main ( String[]args){
 	CarClass Mycar = new CarClass();
 	CarClass Hiscar = new CarClass("Honda","Black",3.5,400);
 	CarClass Hercar = new CarClass("Volks", "Pink",2.5,300);
-	System.out.println("My car'a detail: "+"\n"+Mycar.toString());
-	System.out.println("\nHis car'a detail: "+"\n"+Hiscar.toString());
-	System.out.println("\nHer car'a detail: "+"\n"+Hercar.toString());
+	JOptionPane.showConfirmDialog(null,"My car'a detail: "+"\n"+Mycar.toString());
+	JOptionPane.showConfirmDialog(null,"\nHis car'a detail: "+"\n"+Hiscar.toString());
+	JOptionPane.showConfirmDialog(null,"\nHer car'a detail: "+"\n"+Hercar.toString());
 	
 	Mycar.setMake("Ferrari");
 	Mycar.setColor("Blue");
@@ -24,9 +25,10 @@ public static void main ( String[]args){
 	Hercar.setEngineSize(6.5);
 	Hercar.setHorsePower(650);
 	
-	System.out.println("My car's details after setting vlues"+ "\nName: " + Mycar.getmake()+ "\ncolor: "+ Mycar.getColor()+ "\nHorsePower: " + Mycar.getHorsePower()+ "\nEngineSize: "+ Mycar.getEngineSize());
-	System.out.println("\nHis car's details after setting vlues"+ "\nName: " + Hiscar.getmake()+ "\ncolor: "+ Hiscar.getColor()+ "\nHorsePower: " + Hiscar.getHorsePower()+ "\nEngineSize: "+ Hiscar.getEngineSize());
-	System.out.println("\nHer car's details after setting vlues"+ "\nName: " + Hercar.getmake()+ "\ncolor: "+ Hercar.getColor()+ "\nHorsePower: " + Hercar.getHorsePower()+ "\nEngineSize: "+ Hercar.getEngineSize());
-
+	JOptionPane.showConfirmDialog(null, "My car's details after setting vlues"+ "\nName: " + Mycar.getmake()+ "\ncolor: "+ Mycar.getColor()+ "\nHorsePower: " + Mycar.getHorsePower()+ "\nEngineSize: "+ Mycar.getEngineSize());
+	JOptionPane.showConfirmDialog(null, "\nHis car's details after setting vlues"+ "\nName: " + Hiscar.getmake()+ "\ncolor: "+ Hiscar.getColor()+ "\nHorsePower: " + Hiscar.getHorsePower()+ "\nEngineSize: "+ Hiscar.getEngineSize());
+	JOptionPane.showConfirmDialog(null, "\nHer car's details after setting vlues"+ "\nName: " + Hercar.getmake()+ "\ncolor: "+ Hercar.getColor()+ "\nHorsePower: " + Hercar.getHorsePower()+ "\nEngineSize: "+ Hercar.getEngineSize());
+	
+	JOptionPane.showConfirmDialog(null, "Vehicle count: " +CarClass.getCount());
 }
 }
